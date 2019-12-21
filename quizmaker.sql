7bbf8a7ede788dfdc9f03cb1a6db1cd442f9c25c
Teralios <teralios@posteo.de>
2019-12-21 17:44:06 +0100

    Base sql

---
+++ quizmaker.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS wcf1_quiz;
+CREATE TABLE wcf1_quiz (
+    quizID INT(10) NOT NULL auto_increment PRIMARY KEY,
+    title VARCHAR(255) NOT NULL DEFAULT '',
+    description MEDIUMTEXT,
+    quizType ENUM('fun', 'competition') DEFAULT 'fun',
+    image VARCHAR(255),
+    KEY (quizType)
+);
+
+DROP TABLE IF EXISTS wcf1_quiz_question;
+CREATE TABLE wcf1_quiz_question (
+    questionID INT(10) NOT NULL auto_increment PRIMARY KEY,
+    quizID INT(10) NOT NULL,
+    orderNo SMALLINT(3),
+    question VARCHAR(1000),
+    optionA VARCHAR(1000),
+    optionB VARCHAR(1000),
+    optionC VARCHAR(1000),
+    optionD VARCHAR(1000),
+    answer ENUM('A', 'B', 'C', 'D'),
+    KEY (orderNo)
+);

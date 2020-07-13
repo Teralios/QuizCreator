5f37b3ee7d965c7e82ca85146653bbb22d3fb840
Teralios <teralios@posteo.de>
2020-07-13 16:57:15 +0200

    Start JavaScript Work.

--- files/js/Teralios/Quiz/Quiz.js
+++ files/js/Teralios/Quiz/Quiz.js
@@ -1,5 +1,91 @@
-define(['Ajax', 'Dom/Util'], function (Ajax, Dom) {
+define(['Ajax', 'Dom/Util', 'Language'], function (Ajax, Dom, Language) {
     "use strict";
 
-    return {}
+    var points = [10, 5, 1];
+    var timeLimit = [5, 15, 0];
+    var timeClasses = ['stage0', 'stage1', 'stage2'];
+    var currentTimeStage = 0;
+    var time = 0;
+    var type = 'competition';
+    var data = {};
+    var currentQuestion = 1;
+
+    return {
+        init: function () {
+            this.startButton = elById('quizStart');
+            this.startButton.addEventListener(WCF_CLICK_EVENT, this.startGame.bind(this));
+        },
+
+        startGame: function () {
+            // build quiz game header
+            var headerHtml = '';
+            headerHtml += '<div id="questionCounter"><b>' + Language.get('wcf.quizMaker.play.question') + '</b>';
+            headerHtml += '<span id="currentQuestion"> ' + currentQuestion + '</span> / n</div>';
+            headerHtml += '<div id="questionTime"><b>' + Language.get('wcf.quizMaker.play.time') + '</b> <span id="secondsPlayed"></span></div>';
+            headerHtml += '<div id="questionPoints"></div>';
+
+            // reset vars
+            time = currentQuestion = currentTimeStage = 0;
+
+            // remove start button and add game header
+            elRemove(elById('quizStart'));
+
+            var quizGameHeader = elById('quizGameHeader');
+            quizGameHeader.innerHTML = headerHtml;
+
+            this._gameContent = elById('quizGameContent');
+            this._gameContent.classList.add('borderTop');
+
+
+            // updates counter;
+            this._updatePoints(10);
+            this._updateTime();
+        },
+
+        startCycle: function () {
+            this._updateTime();
+            elById('secondsPlayed').classList.add(timeClasses[currentTimeStage]);
+            elById('questionPoints').innerHTML = '+<b>' + points[currentTimeStage] + '</b> Punkte';
+            setInterval(this.timeWatch.bind(this), 1000);
+        },
+
+        stopCycle: function () {
+            clearInterval(this.timeWatch);
+            time = 0;
+            currentTimeStage = 0;
+
+            this._updateTime();
+            this._updatePoints(points[currentTimeStage])
+        },
+
+        timeWatch: function () {
+            var timeBorder = timeLimit[currentTimeStage];
+
+            if (timeBorder > 0 && time >= timeBorder) {
+                currentTimeStage++;
+                elById('secondsPlayed').classList.remove(timeClasses[currentTimeStage - 1]);
+                elById('secondsPlayed').classList.add(timeClasses[currentTimeStage]);
+                this._updatePoints(points[currentTimeStage])
+            }
+
+            this._updateTime();
+
+            time++;
+        },
+
+        _updatePoints: function (points) {
+            elById('questionPoints').innerHTML = '+' + points + ' <b>' + Language.get('wcf.quizMaker.play.points') + '</b>';
+        },
+
+        _updateTime: function () {
+            var seconds = String(time % 60);
+            var minutes = Math.floor(time / 60)
+
+            if (seconds.length < 2) {
+                seconds = "0" + seconds;
+            }
+
+            elById('secondsPlayed').innerHTML = minutes + ':' + seconds;
+        },
+    }
 });
--- templates/quiz.tpl
+++ templates/quiz.tpl
@@ -31,33 +31,43 @@
         {assign var="media" value=$quiz->getMedia()}
         {if !$media|is_null}
             <div class="quizImage">
-                {@$media->getThumbnailTag('small')}
+                {@$media}
             </div>
         {/if}
         <div class="quizDescription">
             {$quiz->getDescription()}
         </div>
     </div>
-    <div class="quizGame">
-        <div class="quizGameHeader">
-            <div class="questionCounter">Fragen: 1/10</div>
-            <div class="questionTime">Zeit: 0:00</div>
-            <div class="questionScore">+10 Punkte</div>
-        </div>
-        <div class="quizGameContent">
-            <button>{lang}wcf.quizMaker.quiz.start{/lang}</button>
+    <div id="quizGame">
+        <div id="quizGameHeader"></div>
+        <div id="quizGameContent">
+            <button id="quizStart">{lang}wcf.quizMaker.quiz.start{/lang}</button>
 
-            <div class="question">Hier könnte ihre Frage stehen, na wie wäre es?</div>
+            {* <div class="question">Hier könnte ihre Frage stehen, na wie wäre es?</div>
             <ul class="answerList">
                 <li><button class="answerA" disabled>Antwort 1</button></li>
                 <li><button class="answerB" disabled>Antwort 2 Lange</button></li>
                 <li><button class="answerC correctAnswer" disabled>Antwort 3 noch Länger</button></li>
                 <li><button class="answerD wrongAnswer" disabled>Antwort 4 Test</button></li>
-            </ul>
+            </ul> *}
         </div>
-        <div class="quizGameFooter"><button class="nextQuestion">nächste Frage</button></div>
+        <div id="quizGameFooter" class="invisible"><button class="nextQuestion">nächste Frage</button></div>
     </div>
 </div>
+
+<script data-relocate="true">
+    require(['Teralios/Quiz/Quiz', 'Language'], function (Quiz, Language) {
+        Language.addObject({
+            'wcf.quizMaker.play.points': '{lang}wcf.quizMaker.play.points{/lang}',
+            'wcf.quizMaker.play.time': '{lang}wcf.quizMaker.play.time{/lang}',
+            'wcf.quizMaker.play.start': '{lang}wcf.quizMaker.play.start{/lang}',
+            'wcf.quizMaker.play.next': '{lang}wcf.quizMaker.play.next{/lang}',
+            'wcf.quizMaker.play.question': '{lang}wcf.quizMaker.play.question{/lang}'
+        });
+
+        Quiz.init();
+    });
+</script>
 
 
 {include file='footer'}

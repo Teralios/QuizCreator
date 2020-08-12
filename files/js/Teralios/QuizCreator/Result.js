debf4307e198913c1c36d296625972c84f8aa220
Teralios <teralios@posteo.de>
2020-08-12 13:33:10 +0200

    Start work on result view.

--- files/js/Teralios/QuizCreator/Result.js
+++ files/js/Teralios/QuizCreator/Result.js
@@ -3,6 +3,11 @@ define(['StringUtil', 'Language'], function (StringUtil, Language) {
 
     return {
         init: function (playerResult, score, quizData, gameContainer) {
+            this._gameContainer = gameContainer;
+            this._score = score;
+            this._quizData = quizData;
+            this._result = playerResult;
+
             // temp
             gameContainer.innerHTML = '<p>Game finished</p>';
         }
